750e4690806eb8a21b050edb31f54841654ea1a2
Miha Rekar <miha.rekar@gmail.com>
2013-10-21 15:47:52 +0200

    really remove coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,9 +5,6 @@ require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require 'rails/test_help'
 require 'minitest/autorun'
 require 'capybara/rails'
-require 'coveralls'
-
-Coveralls.wear!
 
 Rails.backtrace_cleaner.remove_silencers!
 
