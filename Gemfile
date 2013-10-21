2a896b24fb7661e970839ed3accb03d00baf1cc9
Miha Rekar <miha.rekar@gmail.com>
2013-10-21 15:30:36 +0200

    add coveralls

--- Gemfile
+++ Gemfile
@@ -12,3 +12,6 @@ gemspec
 
 # To use debugger
 # gem 'debugger'
+
+# Coveralls
+gem 'coveralls', require: false
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,12 @@
 ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-require "rails/test_help"
+require 'rails/test_help'
 require 'minitest/autorun'
-require "capybara/rails"
+require 'capybara/rails'
+require 'coveralls'
+
+Coveralls.wear!
 
 Rails.backtrace_cleaner.remove_silencers!
 
